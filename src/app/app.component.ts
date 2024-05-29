59f277fbda8dc0d49f4030dafa40a67a8a3a07bb
Lesx33 <mokiks979@gmail.com>
2024-05-29 10:44:03 -0600

    Agregando formulario

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Injectable } from '@angular/core';
+import { TmdbService } from '../data.services';
 import { RouterOutlet } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-root',
@@ -8,6 +11,24 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ProyectoTwins';
+  popularMovies: any[] = [];
+  popularTVShows: any[] = [];
+
+  constructor(private tmdbService: TmdbService) { }
+
+  ngOnInit() {
+    this.tmdbService.getPopularMovies()
+      .subscribe(movies => {
+        this.popularMovies = movies;
+      });
+
+    this.tmdbService.getPopularTV()
+      .subscribe(tvShows => {
+        this.popularTVShows = tvShows;
+      });
+    console.log(this.popularMovies);
+    console.log(this.popularTVShows);
+  }
 }
