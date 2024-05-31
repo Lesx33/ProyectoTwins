9d4e79a9e1d3635993c10d6a649c7678a90ebe58
Lesx33 <mokiks979@gmail.com>
2024-05-31 07:35:07 -0600

    Resolviendo conflicto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit, Injectable } from '@angular/core';
-import { TmdbService } from '../data.services';
 import { RouterOutlet } from '@angular/router';
-import { HttpClient } from '@angular/common/http';
 
 
 @Component({
@@ -12,23 +10,9 @@ import { HttpClient } from '@angular/common/http';
   styleUrl: './app.component.css'
 })
 export class AppComponent implements OnInit {
-  title = 'ProyectoTwins';
-  popularMovies: any[] = [];
-  popularTVShows: any[] = [];
 
-  constructor(private tmdbService: TmdbService) { }
 
-  ngOnInit() {
-    this.tmdbService.getPopularMovies()
-      .subscribe(movies => {
-        this.popularMovies = movies;
-      });
+  constructor() { }
 
-    this.tmdbService.getPopularTV()
-      .subscribe(tvShows => {
-        this.popularTVShows = tvShows;
-      });
-    console.log(this.popularMovies);
-    console.log(this.popularTVShows);
-  }
+  ngOnInit() {}
 }
